41951fdf78bb99db931004abdb694f29b7e9f67f
ibrahim013 <walaibrahim13@gmail.com>
2018-09-07 09:42:10 +0100

    login-logout functionality added

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,15 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
-<<<<<<< HEAD
 import { Provider } from "react-redux";
 import "./App.css";
 import authCheck from './utilities/authCheck';
 import {setUser} from './actions/authAction';
 import isEmpty from './validator/is-empty';
-=======
-import { Provider } from 'react-redux'
-import "./App.css";
->>>>>>> signup action added
-import store from "./store";
-import LandingPage from "./component/layout/Landing";
-import Navbar from "./component/layout/Navbar";
-import Register from "./component/auth/Register";
-import Login from "./component/auth/Login";
-import Dashboard from "./component/Dashboard";
-
 
 
 if(!isEmpty(authCheck())){
   store.dispatch(setUser(authCheck()));
 }
-
-
 
 class App extends Component {
   render() {
